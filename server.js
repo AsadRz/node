7328092db57f4a805f6da9bea784854f2384f027
Asad <asad.riaz@pwc.com>
2021-12-29 21:00:45 +0500

    Creating servers with req and response syntax

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.json("Hello World");
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello From Node</h1></body>");
+  res.write("</html>");
+  //   res.json("Hello World");
+  res.end();
 });
 
 server.listen(PORT, () => {
