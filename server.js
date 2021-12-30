3385a7663fb4c724fdf9340810c3bf2028e18940
Asad <asad.riaz@pwc.com>
2021-12-30 16:13:14 +0500

    Adding Request listeners to use stream and buffer concepts to read data and storing in an file

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello From Node</h1></body>");
-  res.write("</html>");
-  //   res.json("Hello World");
-  res.end();
+  const { url, method } = req;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<body><form action='/message' method='post'><input type='text' name='message' /><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 });
 
 server.listen(PORT, () => {
